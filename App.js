6fb554b73e56cb8052ea0dc682c9d418ce4d7ded
nightwarrior-xxx <amanv1999@gmail.com>
2020-01-23 16:51:07 +0530

    Pure React

---
+++ App.js
@@ -0,0 +1,30 @@
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt-me"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Mixed"
+    }),
+    React.createElement(Pet, {
+      name: "Peeper",
+      animal: "Bird",
+      breed: "Stale"
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mixed"
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
