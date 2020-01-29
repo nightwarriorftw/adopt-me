9906043108c6d8a9dfdf04af481c5b0206fb7562
nightwarrior-xxx <amanv1999@gmail.com>
2020-01-29 08:44:30 +0530

    Add parcel, extract Pet component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from './Pet';
 
 const App = () => {
   return React.createElement("div", {}, [
@@ -17,7 +13,7 @@ const App = () => {
     React.createElement(Pet, {
       name: "Peeper",
       animal: "Bird",
-      breed: "Stale"
+      breed: "Stalee"
     }),
     React.createElement(Pet, {
       name: "Doink",
@@ -27,4 +23,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
