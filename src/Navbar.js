6d767e1edfa8116eefb7a46c03d403e4775a4cdd
nightwarrior-xxx <amanv1999@gmail.com>
2020-06-28 18:27:45 +0530

    Added animations and lazy loding

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from '@reach/router';
-import { css } from '@emotion/core';
+import { css, keyframes } from '@emotion/core';
+import colors from './colors';
 
-const Navbar = () => (
-    <header
-        css={css`
-                    background-color: #333;
-                    padding: 15px;
+const Navbar = () => {
+    const spin = keyframes`
+        to {
+            transform: rotate(360deg);2
+        }
+    `;
+    const [padding, setPadding] = useState(15)
+    return (
+        <header onClick={() => setPadding(padding + 15)}
+            css={css`
+                    background-color: ${colors.dark};
+                    padding: ${padding}px;
                 `}
-    >
-        <Link to='/'>Adopt-me</Link>
-        <span role='img' aria-label='logo'>Logo</span>
-    </header>
-)
-
+        >
+            <Link to='/'>Adopt-me</Link>
+            <span role='img' aria-label='logo'
+                css={css`
+                    font-size: 60px;
+                    &:hover{
+                        text-decoration: underline;
+                        cursor: pointer;
+                        animation: 1s ${spin} linear infinite;
+                    }
+                `}
+            >🕊️</span>
+        </header>
+    )
+}
 
 export default Navbar;
 
